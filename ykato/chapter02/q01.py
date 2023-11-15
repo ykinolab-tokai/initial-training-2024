6c59b3ee560ce414969b2d51f7f85a0045371960
ykato <ad30326r@gmail.com>
2023-11-15 12:54:29 +0900

    add chapter02/q01

---
+++ ykato/chapter02/q01.py
@@ -0,0 +1,19 @@
+a = [4, 8, 3, 4, 1]
+res = len(a)
+print(res)
+
+a = [4, 8, 3, 4, 1]
+res = max(a)
+print(res)
+
+a = [4, 8, 3, 4, 1]
+res = min(a)
+print(res)
+
+a = [4, 8, 3, 4, 1]
+res = sum(a)
+print(res)
+
+a = [4, 8, 3, 4, 1]
+res = sorted(a)
+print(res)
