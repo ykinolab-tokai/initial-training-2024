6fd0badd75d53f300000599c6b4bc50ef096c7d6
qliu <ningqi1021@gmail.com>
2023-11-22 18:50:50 +0900

    chapter02内容

---
+++ RrryuOvO/chapter02/q05.py
@@ -0,0 +1,8 @@
+numbers = list(map(str, range(100)))
+result_1 = ' '.join(numbers)
+print(result_1)
+
+
+float_value = 1.0 / 7.0
+result_2 = '{:.9f}'.format(float_value)
+print(result_2)
---
+++ RrryuOvO/chapter02/q08.py
@@ -0,0 +1,11 @@
+for num in range(2, 101):
+    is_prime = True
+
+   
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num)
