9f56622ac38e9018417eab3a51a13bdddf6b7766
ykato <ad30326r@gmail.com>
2023-11-22 17:36:03 +0900

    add chapter02

--- ykato/chapter02/q08.py
+++ ykato/chapter02/q08.py
@@ -1,10 +1,7 @@
 for p in range(2, 101):
-    i = 0
-    if p >= 2:
-        for k in range(2, p-1):
-            if k >= 2:
-                a = p % k
-                if a != 0:
-                    i += 1
-                    if i == p-3:
-                        print(p)
+    for k in range(2, p-1):
+        a = p % k
+        if a == 0:
+            break
+    else:
+        print(p)
