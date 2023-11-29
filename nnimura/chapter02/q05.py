e4d519771070fd69a54a604c666ade0de989e28f
nnimura <nimunao226@gmail.com>
2023-11-29 18:03:46 +0900

    第一章の再提出です

---
+++ nnimura/chapter02/q05.py
@@ -0,0 +1,6 @@
+#(1)
+result = ' 'join(str(x)for x in range(100))
+print(result)
+
+#(2)
+"{:.9f}".format(1.0 / 7.0)
---
+++ sample.py
@@ -0,0 +1,7 @@
+array = [50,80,40]
+s = 0
+s += array[0]
+s += array[1]
+s += array[2]
+s /= 3
+print(s)
