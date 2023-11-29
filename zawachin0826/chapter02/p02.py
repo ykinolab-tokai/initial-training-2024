c44a2e343434dd6ec4ac4d977bd7330e26ff4367
zawachin0826 <schwarz2002826@gmail.com>
2023-11-29 17:47:03 +0900

    2章の課題

---
+++ zawachin0826/chapter02/p02.py
@@ -0,0 +1,23 @@
+- 1.2 + 3.8
+結果:5.0
+型:float
+
+- 10 // 100
+結果:0
+型:int
+
+- 1 >= 0
+結果:True
+型:bool
+
+- 'Hello World' == 'Hello World'
+結果:True
+型:bool
+
+- 'Chainer' != 'Tutorial'
+結果:False
+型:bool
+
+- all([True, True, False])
+結果:False
+型:bool
