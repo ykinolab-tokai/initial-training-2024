6e6d0534455b44b04c56bdc5268fe111ff4d15ef
ykato <ad30326r@gmail.com>
2023-12-06 15:40:32 +0900

    add chapter03

--- ykato/chapter03/q05.py
+++ ykato/chapter03/q05.py
@@ -4,7 +4,9 @@ a = ['θ[$^\circ$]', 'sinθ', 'cosθ', 'tanθ']
 b = np.arange(1, 361, 1)
 c = np.full((361, 3), 0.)
 
-for i in range(361):
+print(a)
+
+for i in range(360):
     x = ((2 * np.pi) / 360) * i
 
     c[i, 0] = np.sin(x)
