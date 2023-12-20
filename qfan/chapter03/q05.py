61400857a3e8327e14762819963f4685171a1f30
qfan <3232751584fqz@gmail.com>
2023-12-20 18:28:42 +0900

    homework

--- qfan/chapter03/q05.py
+++ qfan/chapter03/q05.py
@@ -1,12 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 x = np.linspace(0, 4 * np.pi, 360)
-print(x)
-row=[x for x in range(361)]
 y1 = np.sin(x)
 y2 = np.cos(x)
 y3 = np.tan(x)
-print("\t sin\t cos\ttan\t")
+print(" \tsin\tcos\ttan\t")
 for i in range(360):
-    print(f"{i}\t{round(y1[i],5)}\t{round(y2[i],5)}\t{round(y3[i],5)}\t")
+    print(f"{i}\t{format(y1[i],'.5f')} \t{format(y2[i],'.5f')} \t{format(y3[i],'.5f')}")
     print()
