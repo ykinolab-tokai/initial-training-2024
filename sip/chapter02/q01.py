aa27828aaceacb0cf0d1f65fbb25a227c4a4c6d2
muel1126 <muel@DESKTOP-259KOQK.localdomain>
2024-10-09 19:10:38 +0900

    python

---
+++ sip/chapter02/q01.py
@@ -0,0 +1,16 @@
+a=[4, 8, 3, 4, 1]
+
+def Len(l):
+    return len(l)
+def max_v(l):
+    return max(l)
+def min_v(l):
+    return min(l)
+def total(l):
+    return sum(l)
+def Sort(l):
+    return sorted(l)
+
+res = Sort(a)
+print(res)
+
