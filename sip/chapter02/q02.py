93fbc4407a4110a869b7f14529f37edfe22c1a9d
muel1126 <muel@DESKTOP-259KOQK.localdomain>
2024-10-09 19:15:37 +0900

    python

---
+++ sip/chapter02/q02.py
@@ -0,0 +1,9 @@
+print(1.2 + 3.8) #5.0
+print(10 // 100) #0
+print(1 >= 0)  #True
+print('Hello World' == 'Hello World') #True
+print(not 'Chainer' != 'Tutorial') #False
+print(all([True, True, False])) #False
+print(any([True, True, False])) #True
+print(abs(-3))  #3
+#2 // 0  #error
