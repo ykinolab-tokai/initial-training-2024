8c6c242e5a870d6a905dfddb1a74737ddd9813f4
Takato TANNO <2CDI2123@gmail.com>
2024-10-15 15:31:28 +0900

    ２章を追加

---
+++ ttanno/chapter02/q01.py
@@ -0,0 +1,14 @@
+a = [4,8,3,4,1]
+
+res_lengh = len(a)
+print(res_lengh)
+res_max = max(a)
+print(res_max)
+res_min = min(a)
+print(res_min)
+res_sum= sum(a)
+print(res_sum)
+res_sorted = sorted(a)
+print(res_sorted)
+
+
