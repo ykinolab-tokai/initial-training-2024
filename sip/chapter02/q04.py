80d37cc9e3d630b56054922f3c2548dbb0d17d1d
muel1126 <muel@DESKTOP-259KOQK.localdomain>
2024-10-16 15:38:20 +0900

    q04

--- sip/chapter02/q04.py
+++ sip/chapter02/q04.py
@@ -2,11 +2,7 @@ a=[4, 8, 3, 4, 1]
 a = [0 if i%2 == 0 else 1 for i in a]
 print(a)
 
-count=0
-for i in a:
-    if i == 1:
-        count+=1
-print(count)
+print(sum(a))
 
 a=[4, 8, 3, 4, 1]
 a = [i for i in a if i%2 ==1]
