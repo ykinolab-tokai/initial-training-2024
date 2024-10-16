eeba2132faf770b80444d9108bad281b68d9a178
muel1126 <muel@DESKTOP-259KOQK.localdomain>
2024-10-16 15:45:05 +0900

    q02

--- sip/chapter02/q02.py
+++ sip/chapter02/q02.py
@@ -1,9 +1,9 @@
-print(1.2 + 3.8) #5.0
-print(10 // 100) #0
-print(1 >= 0)  #True
-print('Hello World' == 'Hello World') #True
-print(not 'Chainer' != 'Tutorial') #False
-print(all([True, True, False])) #False
-print(any([True, True, False])) #True
-print(abs(-3))  #3
+print(type(1.2 + 3.8)) #5.0 #float
+print(type(10 // 100)) #0  #int
+print(type(1 >= 0)) #True  #boolean
+print(type('Hello World' == 'Hello World')) #True  #boolean
+print(type(not 'Chainer' != 'Tutorial')) #False  #boolean
+print(type(all([True, True, False]))) #False  #boolean
+print(type(any([True, True, False]))) #True  #boolean
+print(type(abs(-3))) #3  #int
 #2 // 0  #error
