e363c21029e1c943c6a8a1dd629c6cf33c7b16c8
yuma3523 <yuma.k060606@gmail.com>
2024-10-16 15:45:34 +0900

    02章課題追加2

--- ykaneko/chapter02/q02.py
+++ ykaneko/chapter02/q02.py
@@ -1,36 +1,36 @@
 a = 1.2 + 3.8 
 #予想　値：5.0　型：float
 print(a)
-type(a)
+print(type(a))
 b = 10 // 100
 #予想　値：0　型：int
 print(b)
-type(b)
+print(type(b))
 c = 1 >= 0
 #予想　値：True　型：bool
 print(c)
-type(c)
+print(type(c))
 d = 'Hello World' == 'Hello World'
 #予想　値：True　型：bool
 print(d)
-type(d)
+print(type(d))
 e = not 'Chainer' != 'Tutorial'
 #予想　値：False　型：bool
 print(e)
-type(e)
+print(type(e))
 f = all([True, True, False])
 #予想　値：False　型：bool
 print(f)
-type(f)
+print(type(f))
 g = any([True, True, False])
 #予想　値：True　型：bool
 print(g)
-type(g)
+print(type(g))
 h = abs(-3)
 #予想　値：3　型：int
 print(h)
-type(h)
+print(type(h))
 i = 2 // 0
 #予想　2を0で割ることができないのでエラーが起こる。
 print(i)
-type(i)
+print(type(i))
--- ykaneko/chapter02/q04.py
+++ ykaneko/chapter02/q04.py
@@ -5,7 +5,7 @@ print(a)
 #(2)
 a = [4, 8, 3, 4, 1]
 a = [0 if x % 2 == 0 else 1 for x in a]
-f'リストaに含まれる奇数の個数は{sum(a)}個です。'
+print(f'リストaに含まれる奇数の個数は{sum(a)}個です。')
 #(3)
 a = [4, 8, 3, 4, 1]
 a = [x for x in a if x % 2 != 0]
