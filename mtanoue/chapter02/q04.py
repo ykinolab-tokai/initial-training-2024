f643a929f5cace78c481b47fbedd921366dee1f3
TanoueMachi <mati0301@icloud.com>
2024-10-16 15:54:24 +0900

    Update q04.py

--- mtanoue/chapter02/q04.py
+++ mtanoue/chapter02/q04.py
@@ -1,7 +1,9 @@
 a = [4, 8, 3, 4, 1]
-result = [0 if x % 2 == 0 else 1 for x in a]
-print(result)
-count = sum(1 for x in a if x % 2 != 0)
+data = [x % 2 for x in a]
+print(data)
+
+count = sum(data)
 print(count)
+
 data = [x for x in a if x % 2 != 0]
 print(data)
