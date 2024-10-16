ab9fbcbd6d7427cbcc83afac2573c1267ba207e8
muel1126 <muel@DESKTOP-259KOQK.localdomain>
2024-10-16 16:55:40 +0900

    q01-04

---
+++ sip/chapter03/q03.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.zeros(3,3)
+print(a)
+b = np.ones(3,3)
+print(b)
+c = np.random.rand(3,3)
+print(c)
+d = np.array([2,0,0]
+             [0,2,0]
+             [0,0,2])
+print(d)
---
+++ sip/chapter03/q04.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+a = np.array(20).reshape(5,4)
+print(a)
