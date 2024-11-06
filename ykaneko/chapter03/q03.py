9a006214a64efc0be5bb67a07434dd82b8596c34
yuma3523 <yuma.k060606@gmail.com>
2024-11-06 12:41:52 +0900

    03章課題追加2

--- ykaneko/chapter03/q03.py
+++ ykaneko/chapter03/q03.py
@@ -4,14 +4,17 @@ import numpy as np
 c1 = np.zeros((3,3)) 
 #np.zeros 関数を使用
 print(c1)
+
 # すべての要素が1の3x3配列
 c2 = np.ones((3,3))
 #np.ones 関数を使用
 print(c2)
+
 # -1から1の間のランダムな要素の3x3配列
 c3 = np.random.uniform(-1,1,(3,3))
 #np.random.uniform(-1, 1, (3, 3)) 関数を使用
 print(c3)
+
 # 対角要素が2で、他の要素が0の3x3配列
 c4 = np.diag([2,2,2])
 #np.diag([2,2,2])関数を使用　指定した対角要素を持つ対角行列を作成
