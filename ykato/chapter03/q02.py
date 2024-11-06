10fdbc984b919724c2d2c383fee0c2a9c72a5fda
ykato <ad30326r@gmail.com>
2024-11-06 15:25:59 +0900

    3章1～4を追加

---
+++ ykato/chapter03/q02.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([-6, -3, 0, 3, 6, 9])
+
+b = a[:]
+
+print(f"要素数:{b}")
---
+++ ykato/chapter03/q03.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a = np.zeros((3, 3))
+print("全ての要素が0")
+print(a)
+
+b = np.ones((3, 3))
+print("全ての要素が1")
+print(b)
+
+c = np.random.default_rng()
+print("-1から1の間のランダムな要素")
+print(c.uniform(-1, 1, (3, 3)))
+
+d1 = np.eye(3)
+d2 = np.eye(3)
+dsum = d1 + d2
+print("対角の要素（0行0列成分，1行1列成分，2行2列成分）が2で，他の要素が0")
+print(dsum)
